dd86c3ff3ff6303f06ac80c131aae9dff526417f
rzak23 <reza.akbar2396@gmail.com>
2021-05-05 13:05:27 +0800

    fixed height window

--- main.js
+++ main.js
@@ -6,14 +6,15 @@ function createWindow () {
     transparent: true,
     frame: false,
     width: 1000,
-    height: 800,
+    height: 600,
     icon: __dirname+'/build/icon.png',
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
     }
   })
 
-  win.loadFile('index.html')
+  win.loadFile('index.html'); 
 }
 
 app.whenReady().then(() => {
@@ -26,7 +27,7 @@ app.whenReady().then(() => {
   })
 })
 
-app.on('window-all-closed', () => {
+  app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
